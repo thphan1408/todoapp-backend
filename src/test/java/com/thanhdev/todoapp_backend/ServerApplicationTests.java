6c8c71331757368b12ce0bf7618da8f3adea2a29
Thành Phan <th.phan1408@gmail.com>
2025-05-24 10:20:07 +0700

    initial commit

---
+++ src/test/java/com/thanhdev/todoapp_backend/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhdev.todoapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
